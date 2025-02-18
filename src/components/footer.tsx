00fcfc4b00b8fea64b4888351317321f6065a9fa
Dorian Grasset <dorian.grasset.contact@gmail.com>
2025-02-18 18:16:10 +0100

    ui: update responsive footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -82,80 +82,84 @@ export default function Footer() {
 
   return (
     <footer className="flex flex-row justify-between rounded-b-md p-4 border-x border-b border-input gap-4">
-      <div ref={divRef} className="flex flex-row gap-4">
-        <Input value={filename} onChange={(e) => setFilename(e.target.value)} placeholder="Name your merged config" className="w-fit" />{' '}
-        <TooltipShortcut tooltip={'Merge'} shortcuts={['Ctrl', 'S']}>
-          <TooltipShortcutTrigger>
-            <Button variant="secondary" size={'default-icon'} className="text-base md:text-sm" onClick={handleMerge} disabled={isMergeDisabled}>
-              <FoldHorizontalIcon />
-              <span className="hidden lg:flex">Merge</span>
-            </Button>
-          </TooltipShortcutTrigger>
-        </TooltipShortcut>
-        <TooltipShortcut tooltip={'Download'} shortcuts={['Ctrl', 'D']}>
-          <TooltipShortcutTrigger>
-            <Button variant="secondary" size={'default-icon'} className="text-base md:text-sm" onClick={handleDownload} disabled={isDownloadCopyDisabled}>
-              <DownloadIcon />
-              <span className="hidden lg:flex">Download</span>
-            </Button>
-          </TooltipShortcutTrigger>
-        </TooltipShortcut>
-        <TooltipShortcut tooltip={'Copy'} shortcuts={['Ctrl', 'Shft', 'C']}>
-          <TooltipShortcutTrigger>
-            <Button variant="secondary" size={'default-icon'} className="text-base md:text-sm" onClick={handleCopy} disabled={isDownloadCopyDisabled}>
-              <CopyIcon />
-              <span className="hidden lg:flex">Copy</span>
-            </Button>
-          </TooltipShortcutTrigger>
-        </TooltipShortcut>
+      <div ref={divRef} className="flex flex-col sm:flex-row gap-4 w-full">
+        <Input value={filename} onChange={(e) => setFilename(e.target.value)} placeholder="Name your merged config" className="w-full sm:w-fit" />
+        <div className="flex flex-row gap-4 justify-normal sm:justify-between w-full">
+          <div className="flex gap-4">
+            <TooltipShortcut tooltip={'Merge'} shortcuts={['Ctrl', 'S']}>
+              <TooltipShortcutTrigger>
+                <Button variant="secondary" size={'default-icon'} className="text-base md:text-sm" onClick={handleMerge} disabled={isMergeDisabled}>
+                  <FoldHorizontalIcon />
+                  <p className="hidden lg:flex">Merge</p>
+                </Button>
+              </TooltipShortcutTrigger>
+            </TooltipShortcut>
+            <TooltipShortcut tooltip={'Download'} shortcuts={['Ctrl', 'D']}>
+              <TooltipShortcutTrigger>
+                <Button variant="secondary" size={'default-icon'} className="text-base md:text-sm" onClick={handleDownload} disabled={isDownloadCopyDisabled}>
+                  <DownloadIcon />
+                  <p className="hidden lg:flex">Download</p>
+                </Button>
+              </TooltipShortcutTrigger>
+            </TooltipShortcut>
+            <TooltipShortcut tooltip={'Copy'} shortcuts={['Ctrl', 'Shft', 'C']}>
+              <TooltipShortcutTrigger>
+                <Button variant="secondary" size={'default-icon'} className="text-base md:text-sm" onClick={handleCopy} disabled={isDownloadCopyDisabled}>
+                  <CopyIcon />
+                  <p className="hidden lg:flex">Copy</p>
+                </Button>
+              </TooltipShortcutTrigger>
+            </TooltipShortcut>
+          </div>
+          <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+            <TooltipShortcut tooltip={'Help'} shortcuts={['Ctrl', 'H']}>
+              <TooltipShortcutTrigger>
+                <DialogTrigger asChild>
+                  <Button ref={buttonRef} size={'default-icon'} variant="secondary">
+                    <CircleHelpIcon />
+                    <p className="hidden lg:flex">Help</p>
+                  </Button>
+                </DialogTrigger>
+              </TooltipShortcutTrigger>
+            </TooltipShortcut>
+            <DialogContent className="sm:max-w-[425px] gap-0">
+              <DialogHeader className="space-y-2">
+                <DialogTitle asChild>
+                  <h1 className="text-xl font-bold">Kubemgr</h1>
+                </DialogTitle>
+                <DialogDescription asChild>
+                  <h2 className="text-base">Merge Kubernetes configs securely in your browser, powered by WebAssembly</h2>
+                </DialogDescription>
+              </DialogHeader>
+
+              <article className="space-y-4 pt-4">
+                <section className="space-y-2">
+                  <h3 className="font-medium">What is Kubemgr?</h3>
+                  <p className="text-sm text-muted-foreground">
+                    Kubemgr is a powerful tool that brings the capabilities of the official Kubernetes configuration management tools directly to your browser. It enables seamless kubeconfig merging
+                    without any installation required.
+                  </p>
+                </section>
+
+                <section className="space-y-2">
+                  <h3 className="font-medium">Key Features</h3>
+                  <ul className="text-sm text-muted-foreground list-disc pl-4 space-y-1">
+                    <li itemProp="featureList">100% client-side processing using WebAssembly technology</li>
+                    <li itemProp="featureList">Identical functionality to the CLI version</li>
+                    <li itemProp="featureList">Enhanced security: all operations performed locally in your browser</li>
+                  </ul>
+                </section>
+
+                <section className="pt-2">
+                  <a href="https://github.com/dorian-grst/kubemgr" target="_blank" className="inline-flex items-center text-sm text-orange-500 hover:text-orange-600 font-medium">
+                    ⭐ Support the project on GitHub <ExternalLink className="ml-1 h-4 w-4" />
+                  </a>
+                </section>
+              </article>
+            </DialogContent>
+          </Dialog>
+        </div>
       </div>
-      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-        <TooltipShortcut tooltip={'Help'} shortcuts={['Ctrl', 'H']}>
-          <TooltipShortcutTrigger>
-            <DialogTrigger asChild>
-              <Button ref={buttonRef} size={'default-icon'} variant="secondary">
-                <CircleHelpIcon />
-                <span className="hidden lg:flex">Help</span>
-              </Button>
-            </DialogTrigger>
-          </TooltipShortcutTrigger>
-        </TooltipShortcut>
-        <DialogContent className="sm:max-w-[425px] gap-0">
-          <DialogHeader className="space-y-2">
-            <DialogTitle asChild>
-              <h1 className="text-xl font-bold">Kubemgr</h1>
-            </DialogTitle>
-            <DialogDescription asChild>
-              <h2 className="text-base">Merge Kubernetes configs securely in your browser, powered by WebAssembly</h2>
-            </DialogDescription>
-          </DialogHeader>
-
-          <article className="space-y-4 pt-4">
-            <section className="space-y-2">
-              <h3 className="font-medium">What is Kubemgr?</h3>
-              <p className="text-sm text-muted-foreground">
-                Kubemgr is a powerful tool that brings the capabilities of the official Kubernetes configuration management tools directly to your browser. It enables seamless kubeconfig merging
-                without any installation required.
-              </p>
-            </section>
-
-            <section className="space-y-2">
-              <h3 className="font-medium">Key Features</h3>
-              <ul className="text-sm text-muted-foreground list-disc pl-4 space-y-1">
-                <li itemProp="featureList">100% client-side processing using WebAssembly technology</li>
-                <li itemProp="featureList">Identical functionality to the CLI version</li>
-                <li itemProp="featureList">Enhanced security: all operations performed locally in your browser</li>
-              </ul>
-            </section>
-
-            <section className="pt-2">
-              <a href="https://github.com/dorian-grst/kubemgr" target="_blank" className="inline-flex items-center text-sm text-orange-500 hover:text-orange-600 font-medium">
-                ⭐ Support the project on GitHub <ExternalLink className="ml-1 h-4 w-4" />
-              </a>
-            </section>
-          </article>
-        </DialogContent>
-      </Dialog>
     </footer>
   )
 }
--- src/components/tooltip-shortcut.tsx
+++ src/components/tooltip-shortcut.tsx
@@ -23,11 +23,11 @@ export default function TooltipShortcut({ children, tooltip, shortcuts }: Toolti
         {children}
         <TooltipContent>
           <div className="flex flex-row gap-2 justify-center items-center">
-            <span>{tooltip}</span>
+            <p>{tooltip}</p>
             {shortcuts.map((shortcut, index) => (
               <React.Fragment key={`${shortcut}-${index}`}>
                 <Shortcut letter={shortcut} />
-                {index < shortcuts.length - 1 && <span>+</span>}
+                {index < shortcuts.length - 1 && <p>+</p>}
               </React.Fragment>
             ))}
           </div>
